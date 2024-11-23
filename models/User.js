61e8303873e84ed91d203a79652453d2d0cc4c17
SafeSiteDev <adamr@safesitesecurity.org>
2024-11-23 14:20:42 -0600

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'guard',
+  },
+}, { timestamps: true });
+
+module.exports = User;
